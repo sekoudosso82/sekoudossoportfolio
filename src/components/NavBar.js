92978a587c28115e45d6443a08175f679c6d6f25
sekoudosso82 <sekou.dosso82@gmail.com>
2020-06-18 07:53:26 -0400

    testing netlify link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { NavLink } from 'react-router-dom'
-import {About,Blog,Contact,Home,Portfolio} from './Index'
+// import {About,Blog,Contact,Home,Portfolio} from './Index'
 
 import '../App.css';
 // import { withRouter } from "react-router";
