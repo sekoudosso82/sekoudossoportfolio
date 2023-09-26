4b15da1350f4961bd7682120d6344200b41b1772
SekouDosso <sekou.dosso@revature.net>
2023-09-26 10:15:10 -0400

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,6 +23,34 @@ class Home extends Component
                                     full stack development  
                                     by completing a bootcamp for Software Engineering at Flatrion School in New York City in May 2020. 
                                 </p>
+                                <h2> Professional Summary </h2>
+                        <p> I am a Full-stack ASP.NET developer with experience building web applications. I received 
+                            my degree from CUNY Lehman College located in New York City. I love Computer Science 
+                            from my community college so decide to go into IT from my first semester of Community 
+                            college. I have been interested in coding and software development for some time since 
+                            taking some coding classes as an undergraduate, and I have really enjoyed this field. 
+                            I hope to become a senior developer in few years to mentor others. 
+                        </p>
+                        <p> Since deciding to shift careers, I have developed applications using both Node.js 
+                            and C# in a .NET environment. I have an sp built responsive, dynamic, and flexible 
+                            front ends using HTML, CSS, JavaScript, Razor Views, Express Handlebars, Angular, 
+                            and React.  During my time at Revature, I have worked on several projects which 
+                            expanded my skill set with both backend (including .NET, ASP.NET, Entity Framework, 
+                            SQL, and SOA) and frontend technologies (HTML, CSS, JS, Typescript, and Angular). 
+                            I've gained experience deploying my applications to the cloud making use of CI/CD 
+                            tools, primarily Azure DevOps pushed to the Azure cloud. I've enjoyed collaborating 
+                            with the other associations in my batch as we learn more about these technologies 
+                            together and work collaboratively to build quality applications. It has been an 
+                            excellent opportunity to learn more about different perspectives on coding as well 
+                            as how to effectively communicate and plan within a team. For my latest project, 
+                            I was a full-stack developer which involves facilitating the daily standup meetings 
+                            and helping to resolve any blockers my teammates have. I look forward to applying 
+                            all of my skills and prior experience in a position on your team.
+                        </p>
+                        <p>  I'm very quick to grasp new concepts and I have the passion for learning and drive to 
+                            make a valuable contribution to a team. I am fully confident I can leverage my ability 
+                            to pick up new software to be using it for projects very quickly. 
+                        </p>
 
                                 <h3>  
                                     <NavLink  to='/resume'>read more</NavLink> 
